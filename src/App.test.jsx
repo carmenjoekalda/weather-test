5a3e2f9cca846b38b40aa45bd810d41acc492284
Carmen Jõekalda <cjoekalda@gmail.com>
2025-07-17 16:44:52 +0300

    feat: add city to list + show details

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, within, act } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
 import App from "./App";
@@ -19,17 +19,47 @@ describe("Weather Application tests", () => {
         const linkElement = screen.getByText(/Weather Application/i);
         expect(linkElement).toBeInTheDocument();
     });
-    
-    it("shows city search results", async () => {
-    render(<App />);
 
-    const input = screen.getByTestId("search-input")
-    userEvent.type(input, "Melbourne")
+    it("shows city search result", async () => {
+        render(<App />);
 
-    const button = screen.getByTestId("search-button")
-    userEvent.click(button)
+        const input = screen.getByTestId("search-input")
+        userEvent.type(input, "Melbourne")
 
-    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
-});
+        const button = screen.getByTestId("search-button")
+        userEvent.click(button)
+
+        await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+    });
+
+    it("shows city search result details", async () => {
+        render(<App />);
+
+        const input = screen.getByTestId("search-input")
+        userEvent.type(input, "Melbourne")
+
+        const button = screen.getByTestId("search-button")
+        userEvent.click(button)
+
+        await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+        expect(screen.getByText(/Melbourne, -37.8141705, 144.9655616/i)).toBeInTheDocument()
+    });
+
+    it("add search result to my weather list", async () => {
+        render(<App />);
+
+        const input = screen.getByTestId("search-input")
+        userEvent.type(input, "Melbourne")
+
+        const button = screen.getByTestId("search-button")
+        userEvent.click(button)
+
+        await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+
+        const selected = screen.getAllByText(/Melbourne/i)[3]
+        await userEvent.click(selected);
+
+        expect(within(screen.getByTestId("my-weather-list")).getByText(/Melbourne/i)).toBeInTheDocument()
+    });
 })
 
